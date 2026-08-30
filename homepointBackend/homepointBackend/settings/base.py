fe0f64fba5247bfd1276ab391383a8a77bc24633
42ndLight <leichteed@gmail.com>
2026-08-30 20:34:59 +0300

    feat: separate setting file for blue/green deployment

---
+++ homepointBackend/homepointBackend/settings/base.py
@@ -0,0 +1,4 @@
+# base.py
+from pathlib import Path
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
--- homepointBackend/homepointBackend/settings/dev.py
+++ homepointBackend/homepointBackend/settings/dev.py
@@ -1,3 +1,4 @@
+# dev.py
 """
 Django settings for homepointBackend project.
 
@@ -16,12 +17,11 @@ import dj_database_url
 
 from dotenv import load_dotenv
 from decouple import config, Csv
+from .base import *
 
 # Load environment variables from .env file
 load_dotenv()
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
